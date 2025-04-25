7f37dc123ad3104230c67c1c403c1e6aedc0b36b
Kaio Delphino <dash.zet@gmail.com>
2025-04-25 14:42:50 -0300

    chore: make release command do nothing

--- commands/release/release.go
+++ commands/release/release.go
@@ -3,9 +3,6 @@
 package release_command
 
 import (
-	"context"
-	"os"
-
 	"github.com/urfave/cli/v3"
 	"ulist.app/ult/commands/release/bump"
 	"ulist.app/ult/commands/release/create"
@@ -17,26 +14,11 @@ const (
 )
 
 var Cmd = cli.Command{
-	Name:   "release",
-	Usage:  "increment the app version number",
-	Action: run,
+	Name:  "release",
+	Usage: "increment the app version number",
 	Commands: []*cli.Command{
 		&bump.Cmd,
 		&create.Cmd,
 		&deploy.Cmd,
 	},
-}
-
-func run(ctx context.Context, cmd *cli.Command) error {
-	contents, err := os.ReadFile(".secrets/prod/GoogleApplicationCredentials-ulist.json")
-	if err != nil {
-		return err
-	}
-	latestBuild, err := google.GetVersionFromLatestRelease(contents, "app.ulist")
-	if err != nil {
-		return err
-	}
-
-	println(latestBuild)
-	return err
 }
