b652d9422ce5bb57178af5254fbbee3da211075c
Kaio Delphino <dash.zet@gmail.com>
2026-06-30 10:43:13 -0300

    fix: secrets upload was using fixed project id

--- commands/secrets/secrets.go
+++ commands/secrets/secrets.go
@@ -21,11 +21,6 @@ const (
 )
 
 const (
-	secrestsProjectId = "68640226"
-	appProjectId      = "41950965"
-)
-
-const (
 	defaultSecretsFileName = ".secrets.tar.gz"
 )
 
@@ -106,11 +101,16 @@ func listSecureFilesCommand(ctx context.Context, cmd *cli.Command) error {
 		return err
 	}
 
+	projectId, err := core.GetProjectID(cmd)
+	if err != nil {
+		return err
+	}
+
 	targetName := cmd.String(flagFileName)
 	showOnlyId := cmd.Bool(flagId)
 	logger.Info("Fetching secure files", "with name", targetName, "show only id", showOnlyId)
 
-	files, _, err := secrets.FetchAll(appProjectId, token)
+	files, _, err := secrets.FetchAll(projectId, token)
 	if err != nil {
 		return err
 	}
@@ -129,11 +129,16 @@ func deleteSecureFileCommand(ctx context.Context, cmd *cli.Command) error {
 		return err
 	}
 
+	projectId, err := core.GetProjectID(cmd)
+	if err != nil {
+		return err
+	}
+
 	targetName := cmd.String(flagFileName)
 	showOnlyId := cmd.Bool(flagId)
 	logger.Info("Fetching secure files", "with name", targetName, "show only id", showOnlyId)
 
-	files, appRepo, err := secrets.FetchAll(appProjectId, token)
+	files, appRepo, err := secrets.FetchAll(projectId, token)
 	if err != nil {
 		return err
 	}
@@ -143,7 +148,7 @@ func deleteSecureFileCommand(ctx context.Context, cmd *cli.Command) error {
 		return fmt.Errorf("No secure file was found with given name: %s", targetName)
 	}
 
-	err = secrets.Delete(appRepo, file.ID, targetName, appProjectId)
+	err = secrets.Delete(appRepo, file.ID, targetName, projectId)
 	if err != nil {
 		return err
 	}
@@ -154,6 +159,11 @@ func deleteSecureFileCommand(ctx context.Context, cmd *cli.Command) error {
 func updateSecureFileCommand(ctx context.Context, cmd *cli.Command) error {
 	setLoggingVerbosity(cmd.Bool(core.VerboseFlag))
 	token, err := core.GetToken(cmd)
+	if err != nil {
+		return err
+	}
+
+	projectId, err := core.GetProjectID(cmd)
 	if err != nil {
 		return err
 	}
@@ -172,7 +182,7 @@ func updateSecureFileCommand(ctx context.Context, cmd *cli.Command) error {
 	}
 	logger.Info("Using absolute path for archive upload", "path", path)
 
-	files, appRepo, err := secrets.FetchAll(appProjectId, token)
+	files, appRepo, err := secrets.FetchAll(projectId, token)
 	if err != nil {
 		return err
 	}
@@ -185,14 +195,14 @@ func updateSecureFileCommand(ctx context.Context, cmd *cli.Command) error {
 	logger.Info("Found secure file", "id", file.ID, "name", defaultSecretsFileName)
 
 	logger.Info("Deleting existing secure file", "id", file.ID)
-	err = secrets.Delete(appRepo, file.ID, path, appProjectId)
+	err = secrets.Delete(appRepo, file.ID, path, projectId)
 	if err != nil {
 		return err
 	}
 	logger.Info("Successfully deleted secure file", "id", file.ID)
 
-	logger.Info("Creating new secure file", "project_id", appProjectId)
-	err = secrets.Create(appRepo, path, appProjectId)
+	logger.Info("Creating new secure file", "project_id", projectId)
+	err = secrets.Create(appRepo, path, projectId)
 	if err != nil {
 		return err
 	}
